36d942d91d8423c0f23d89fa668c08e3e21a5f9a
ohwin <ohwin@ohwins-Mac-mini.local>
2026-02-12 16:44:02 +0800

    feat(finance): 在提款响应中添加基础响应字段
    - 在 WithdrawResp 消息中添加了 BaseResponse 字段
    - 将 order_no 字段从位置 1 移动到位置 2
    - 为 WithdrawResp 生成了 GetBase 方法
    - 更新了 protobuf 依赖索引以包含 BaseResponse 类型
    - 为 BaseResponse 字段添加了验证逻辑
    - 修改了 proto 文件中的消息定义结构

--- utils/xerr/code.go
+++ utils/xerr/code.go
@@ -48,7 +48,7 @@ const (
 	// ErrCodeUserAbnormalError 用户账号状态异常
 	ErrCodeUserAbnormalError ErrCode = 10011 // UserAbnormalError
 	// ErrCodeGoogleAuthError 谷歌校验失败，请重试
-	ErrCodeGoogleAuthError            ErrCode = 10012 //
+	ErrCodeGoogleAuthError ErrCode = 10012 // GoogleAuthError
 )
 
 // 游戏相关
