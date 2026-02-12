5a7948238c2b0a04dbb27d0b80e99afc331c1225
any <any@hanyouweb.com>
2026-02-12 11:36:09 +0800

    xcode

--- utils/xerr/code.go
+++ utils/xerr/code.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 )
 
-// 通用错误码
 const (
+	// 通用错误码
 	ErrCodeSuccess             ErrCode = 0   // 成功
 	ErrCodeFail                ErrCode = 1   // 失败
 	ErrCodeParamError          ErrCode = 400 // 参数错误
@@ -14,6 +14,21 @@ const (
 	ErrCodeNotFound            ErrCode = 404 // 未找到
 	ErrCodeServerInternalError ErrCode = 500 // 服务器内部错误
 	ErrCodeServiceUnavailable  ErrCode = 503 // 服务不可用
+
+	// 用户相关
+	ErrCodeUserNotExists              ErrCode = 10001 // 用户不存在
+	ErrCodeUserExists                 ErrCode = 10002 // 用户已存在
+	ErrCodeUserPwdError               ErrCode = 10003 // 用户密码错误
+	ErrCodeHadBindError               ErrCode = 10004 // 账号已被绑定
+	ErrCodeNotBindError               ErrCode = 10005 // 账号未被绑定
+	ErrCodeRepeatUpdateError          ErrCode = 10006 // 账号重复更新
+	ErrCodeTemporaryTokenEmptyError   ErrCode = 10007 // 账号更新令牌为空
+	ErrCodeTemporaryTokenInvalidError ErrCode = 10008 // 账号更新令牌无效
+	ErrCodeSelfHadBindError           ErrCode = 10009 // 当前你已绑定该账号
+	ErrCodeBindSameError              ErrCode = 10010 // 换绑账号跟老账号相同
+
+	// 游戏相关
+	ErrCodeGameEnterLogExists ErrCode = 20001 // 用户进入游戏日志已存在
 )
 
 type ErrCode int
