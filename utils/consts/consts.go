fa825c145dc3203b658acefdb61da2518f1b2a36
tanszhe <vic@ogmail.org>
2026-03-13 17:00:52 +0800

    refactor(utils): 优化PtbCoin结构体方法实现
    - 将PtbCoin的方法接收器从指针类型改为值类型
    - 简化NewPtbCoinFromInt函数实现，移除不必要的指针操作
    - 统一方法返回类型，提高代码一致性
    - 移除冗余的指针分配操作，提升性能

--- utils/consts/consts.go
+++ utils/consts/consts.go
@@ -36,22 +36,21 @@ type PtbCoin struct {
 	decimal.Decimal
 } // 平台币
 
-func (p *PtbCoin) Code() string {
+func (p PtbCoin) Code() string {
 	return "PTB"
 }
 
-func (p *PtbCoin) ToDecimal() decimal.Decimal {
+func (p PtbCoin) ToDecimal() decimal.Decimal {
 	return p.Decimal
 }
 
-func (p *PtbCoin) FromDecimal(dcl decimal.Decimal) *PtbCoin {
+func (p PtbCoin) FromDecimal(dcl decimal.Decimal) PtbCoin {
 	p.Decimal = dcl
 	return p
 }
 
-func NewPtbCoinFromInt(val int64) *PtbCoin {
-	p := &PtbCoin{}
-	return p.FromDecimal(decimal.NewFromInt(val))
+func NewPtbCoinFromInt(val int64) PtbCoin {
+	return PtbCoin{}.FromDecimal(decimal.NewFromInt(val))
 }
 
 type OrderPrefix string
