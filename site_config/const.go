62f2ed50099f242798c83937500e56a70b10e6c0
shaw <og@ogdeMac-mini.local>
2026-03-14 15:19:12 +0800

    添加系统配置信息

---
+++ site_config/const.go
@@ -0,0 +1,48 @@
+package site_config
+
+// SiteConfigCategory 站点配置分类
+type SiteConfigCategory int
+
+const (
+	SiteConfigCategoryGame                SiteConfigCategory = 1  // 游戏账号
+	SiteConfigCategoryBaseInfo            SiteConfigCategory = 2  // 站点配置
+	SiteConfigCategoryFooterInfo          SiteConfigCategory = 3  // 页脚配置
+	SiteConfigCategoryDownloadAPP         SiteConfigCategory = 4  // 下载app配置
+	SiteConfigCategorySidebarLoginYes     SiteConfigCategory = 5  // 侧边栏已登录配置
+	SiteConfigCategorySidebarLoginNo      SiteConfigCategory = 6  // 侧边栏未登录配置
+	SiteConfigCategoryFooterMenus         SiteConfigCategory = 7  // 底部主菜单配置
+	SiteConfigCategoryUnLoginPromotionImg SiteConfigCategory = 8  // 未登录宣传图
+	SiteConfigCategoryLobby               SiteConfigCategory = 9  // 大厅
+	SiteConfigCategorySeo                 SiteConfigCategory = 10 // SEO配置
+	SiteConfigThirdLogin                  SiteConfigCategory = 11 // 三方登录
+	SiteConfigCategoryAgent               SiteConfigCategory = 12 // 代理配置
+	SiteConfigCategoryVip                 SiteConfigCategory = 13 // Vip配置
+)
+
+// SiteConfigKey 站点配置key
+type SiteConfigKey string
+
+const (
+	SiteConfigKeyGame                    SiteConfigKey = "game"                      // 游戏账号配置
+	SiteConfigKeyGameFixedCategory       SiteConfigKey = "fixed_game_category"       // 游戏内置分类
+	SiteConfigKeyBaseInfo                SiteConfigKey = "base_info"                 // 站点配置
+	SiteConfigKeyFooterInfo              SiteConfigKey = "footer_info"               // 页脚配置
+	SiteConfigKeyDownloadAPP             SiteConfigKey = "download_app"              // 下载app配置
+	SiteConfigKeyFooterMenus             SiteConfigKey = "footer_menus"              // 底部主菜单配置
+	SiteConfigKeySidebarUserAccount      SiteConfigKey = "sidebar_user_account"      // 用户账号区域
+	SiteConfigKeySidebarRechargeDeposit  SiteConfigKey = "sidebar_recharge_withdraw" // 充值提现区域
+	SiteConfigKeySidebarFeatureOperation SiteConfigKey = "sidebar_feature_operation" // 功能操作区域
+	SiteConfigKeySidebarOtherLinks       SiteConfigKey = "sidebar_other_links"       // 其他链接区域
+	SiteConfigKeySidebarAdPlacement      SiteConfigKey = "sidebar_ad_placement"      // 广告位区域
+	SiteConfigKeySidebarAppDownload      SiteConfigKey = "sidebar_app_download"      // APP下载区域
+	SiteConfigKeySidebarLanguageSwitch   SiteConfigKey = "sidebar_language_switch"   // 语言切换区域
+	SiteConfigKeySidebarVersionInfo      SiteConfigKey = "sidebar_version_info"      // 版本信息区域
+	SiteConfigKeySidebarLoginRegister    SiteConfigKey = "sidebar_login_register"    // 登录注册区域
+	SiteConfigKeyUnLoginHomeConfig       SiteConfigKey = "home_config"               // 首页未登录宣传图
+	SiteConfigKeyLobbyEnter              SiteConfigKey = "lobby_enter"               // 大厅快捷入口
+	SiteConfigKeySeoConfig               SiteConfigKey = "seo_config"                // SEO配置
+	SiteConfigKeyGoogleConfig            SiteConfigKey = "google_config"             // 谷歌登录配置
+	SiteConfigKeyAgentGradeIcons         SiteConfigKey = "agent_grade_icons"         // 代理等级图标
+	SiteConfigKeyVipPromoteInfo          SiteConfigKey = "vip_promote_info"          // Vip宣传图
+	SiteConfigKeyVipCustomerService      SiteConfigKey = "vip_customer_service"      // Vip专属客服
+)
---
+++ site_config/model.go
@@ -0,0 +1,37 @@
+package site_config
+
+// LanguageContent 语言内容信息
+type LanguageContent struct {
+	Lang    string `json:"lang"`    // 语言编码
+	Content string `json:"content"` // 内容
+}
+
+// VipPromoteInfo vip推广信息
+type VipPromoteInfo struct {
+	DefaultLanguage string                  `json:"default_language"`
+	H5              *VipEndpointPromoteInfo `json:"h5"`
+	PC              *VipEndpointPromoteInfo `json:"pc"`
+	APP             *VipEndpointPromoteInfo `json:"app"`
+}
+
+// VipEndpointPromoteInfo vip终端推广信息
+type VipEndpointPromoteInfo struct {
+	Image    string             `json:"image"`    // 图片
+	Contents []*LanguageContent `json:"contents"` // 内容列表
+}
+
+// VipCustomerService vip客服信息
+type VipCustomerService struct {
+	DefaultLanguage string                      `json:"default_language"`
+	H5              *VipEndpointCustomerService `json:"h5"`
+	PC              *VipEndpointCustomerService `json:"pc"`
+	APP             *VipEndpointCustomerService `json:"app"`
+}
+
+// VipEndpointCustomerService vip终端客服信息
+type VipEndpointCustomerService struct {
+	Icon     string             `json:"icon"`      // 图标
+	Link     string             `json:"link"`      // 跳转链接
+	Names    []*LanguageContent `json:"names"`     // 名称列表
+	LinkType int                `json:"link_type"` // 链接类型
+}
