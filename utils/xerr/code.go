f60ba2f22cb9ff535fa8776648d7e7ff17a302bf
oathYc <oathyc@ogmail.org>
2026-04-24 09:42:15 +0800

    错误码

--- utils/xerr/code.go
+++ utils/xerr/code.go
@@ -63,10 +63,8 @@ const (
 	ErrCodeLoginLocked ErrCode = 10018 // LoginLocked
 	// ErrCodeSetPinLockedFirst 设置PIN码失败，账号已锁定（首次锁定）
 	ErrCodeSetPinLockedFirst ErrCode = 10019 // SetPinLockedFirst
-	// ErrCodeOldPinError 旧PIN码错误
+	// ErrCodeOldPinError 旧PIN码错误 带剩余次数
 	ErrCodeOldPinError ErrCode = 10020 // OldPinError
-	// ErrCodeWithdrawLocked 提现功能已被锁定，请稍后再试
-	ErrCodeWithdrawLocked ErrCode = 10021 // WithdrawLocked
 	// ErrCodeSetPinLockedMax 设置PIN码失败，账号已锁定（最大锁定）
 	ErrCodeSetPinLockedMax ErrCode = 10022 // SetPinLockedMax
 	// ErrCodeWithdrawAccountNotExists 提现账号不存在
@@ -96,6 +94,10 @@ const (
 	ErrCodeWithdrawAmountOutOfRange ErrCode = 30002 // WithdrawAmountOutOfRange
 	// ErrCodeRechargeAmountOutOfChannelDayMax 充值金额超出渠道每日最大限制
 	ErrCodeRechargeAmountOutOfChannelDayMax ErrCode = 30003 // ErrCodeRechargeAmountOutOfChannelDayMax
+	// ErrCodeWithdrawLocked 提现功能已被锁定，请稍后再试
+	ErrCodeWithdrawLocked ErrCode = 30004 // WithdrawLocked
+	// ErrCodePinCheckError PIN码错误(发起提现)
+	ErrCodePinCheckError ErrCode = 30005 // PinCheckError
 )
 
 // 代理相关
