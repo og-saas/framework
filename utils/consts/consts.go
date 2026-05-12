66d7f0fcdc8d3cde0cc07bb9205927545b006b79
oathYc <oathyc@ogmail.org>
2026-05-12 10:54:50 +0800

    third type

--- utils/consts/consts.go
+++ utils/consts/consts.go
@@ -1,6 +1,7 @@
 package consts
 
 import (
+	comV1 "github.com/og-saas/proto/pb/common/v1"
 	"github.com/shopspring/decimal"
 	"github.com/zeromicro/go-zero/core/stringx"
 )
@@ -176,4 +177,33 @@ func (d DeviceType) EndpointSubType() EndpointSubType {
 	}
 
 	return EndpointSubTypeOther
+}
+
+// ThirdPartyOauthType 第三方登录类型
+type ThirdPartyOauthType string
+
+const (
+	ThirdPartyOauthTypeGoogle   ThirdPartyOauthType = "google"   // Google
+	ThirdPartyOauthTypeTelegram ThirdPartyOauthType = "telegram" // Telegram
+	ThirdPartyOauthTypeFacebook ThirdPartyOauthType = "facebook" // Facebook
+	ThirdPartyOauthTypeX        ThirdPartyOauthType = "x"        // X
+)
+
+func (t ThirdPartyOauthType) String() string {
+	return string(t)
+}
+
+func (t ThirdPartyOauthType) ToThirdAuthType() comV1.ThirdAccountType {
+	switch t {
+	case ThirdPartyOauthTypeGoogle:
+		return comV1.ThirdAccountType_THIRD_ACCOUNT_TYPE_GOOGLE
+	case ThirdPartyOauthTypeTelegram:
+		return comV1.ThirdAccountType_THIRD_ACCOUNT_TYPE_TELEGRAM
+	case ThirdPartyOauthTypeFacebook:
+		return comV1.ThirdAccountType_THIRD_ACCOUNT_TYPE_FACEBOOK
+	case ThirdPartyOauthTypeX:
+		return comV1.ThirdAccountType_THIRD_ACCOUNT_TYPE_X
+	default:
+		return comV1.ThirdAccountType_THIRD_ACCOUNT_TYPE_UNSPECIFIED
+	}
 }
